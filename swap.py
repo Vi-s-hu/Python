d4831089046467e14ac32f088a926990b6aaaeae
VISHU GOEL <94191687+Vi-s-hu@users.noreply.github.com>
2021-12-07 22:15:52 +0530

    Swap two numbers

---
+++ swap.py
@@ -0,0 +1,7 @@
+x=float(input("ENTER THE VALUE OF X: "))
+y=float(input("ENTER THE VALUE OF Y: "))
+temp = x
+x = y
+y = temp
+print("THE SWAPPED VALUE OF x IS: {}".format(x))
+print("THE SWAPPED VALUE OF y IS: {}".format(y))
